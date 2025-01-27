d6be2b6b07000419f28667707a52ca6c11997889
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-01-27 20:35:24 +0100

    init script full

--- init-repos.sh
+++ init-repos.sh
@@ -1,5 +1,10 @@
 if [ ! -d "/data/.git" ]; then
+  rm -rf /data/*
   git clone --progress --verbose https://github.com/TBMPQF/TBM_CPU_V2.git /data
 else
   echo "Le dépôt existe déjà dans /data"
 fi
+
+mkdir -p /data/logs && touch /data/logs/current_output.log /data/logs/current_error.log &&
+mv /data/logs/current_output.log /data/logs/output.log &&
+mv /data/logs/current_error.log /data/logs/error.log &&
