87f104346c1d1fa94139cc165ccecbf5bc261108
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-01-27 20:46:07 +0100

    init-repos last fix

--- init-repos.sh
+++ init-repos.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # Vérifie si le dépôt git n'existe pas
 if [ ! -d "/data/.git" ]; then
   # Supprime tout dans /data sauf le dossier git
-  rm -rf /data/*
+  rm -rf /data/* /data/.* 2>/dev/null
   # Clone le dépôt git
   git clone --progress --verbose https://github.com/TBMPQF/TBM_CPU_V2.git /data
 else
