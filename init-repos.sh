1f805c3fbe3a2f50615e691a1257bd7ec83831fb
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-04-22 13:53:26 +0200

    Update Docs

--- init-repos.sh
+++ init-repos.sh
@@ -2,7 +2,7 @@
 
 # Vérifie si le dépôt git n'existe pas
 if [ ! -d "/data/.git" ]; then
-  # Supprime tout dans /data sauf le dossier git
+  # Supprime tout dans /data
   rm -rf /data/* /data/.* 2>/dev/null
   # Clone le dépôt git
   git clone --progress --verbose https://github.com/TBMPQF/TBM_CPU_V2.git /data
@@ -14,6 +14,6 @@ fi
 mkdir -p /data/logs
 touch /data/logs/current_output.log /data/logs/current_error.log
 
-# Renomme les fichiers logs
+# Renomme les anciens fichiers logs
 mv /data/logs/current_output.log /data/logs/output.log
 mv /data/logs/current_error.log /data/logs/error.log
