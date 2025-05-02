f9485d5288f9c8cc87da3efc2e7a657b39ef8dd4
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-05-03 01:11:11 +0200

    fix vars mandatory

--- CreateConfig.sh
+++ CreateConfig.sh
@@ -1,11 +1,20 @@
+#!/bin/sh
+
+# Create config.json (deprecated, use the one in TBM_CPU_V2)
 cat << EOF > ./config.json
 {
+  "connection": {
+    "maxAttempts": $RETRY_MAX,
+    "retryDelay": $RETRY_DELAY
+  },
   "token": "$TOKEN",
   "mongourl": "$BDD_URL",
   "serveurMinecraftDOMAIN": "$PERSONAL_DOMAINE_NAME",
   "twitch": {
     "clientId": "$TWITCH_ID",
     "clientSecret": "$TWITCH_SECRET"
-  }
+  },
+  "apex_api": "$APEX_API",
+  "genius_api": "$GENIUS_API"
 }
 EOF
